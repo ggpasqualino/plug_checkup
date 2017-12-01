6909a9817d763156d092225ae1edb1ae527ce9fc
Guilherme <ggpasqualino@gmail.com>
2017-12-01 14:43:34 +0100

    Add tests for PlugCheckup.Options

---
+++ test/plug_checkup/options_test.exs
@@ -0,0 +1,15 @@
+defmodule PlugCheckup.OptionsTest do
+  use ExUnit.Case, async: true
+
+  alias PlugCheckup.Options
+
+  test "it has default timeout" do
+    options = %Options{}
+    assert options.timeout == :timer.seconds(1)
+  end
+
+  test "it has default checks" do
+    options = %Options{}
+    assert options.checks == []
+  end
+end

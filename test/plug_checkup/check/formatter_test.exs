e27d289a6a7cd398f14bc644f0b68165a4628741
Guilherme <ggpasqualino@gmail.com>
2017-12-01 16:49:42 +0100

    Add tests for PlugCheckup.Check.Formatter

---
+++ test/plug_checkup/check/formatter_test.exs
@@ -0,0 +1,68 @@
+defmodule PlugCheckup.Check.FormatterTest do
+  use ExUnit.Case, async: true
+
+  alias PlugCheckup.Check
+  alias PlugCheckup.Check.Formatter
+
+  describe "format/1" do
+    test "it formats successful check" do
+      check =
+        %Check{
+          name: "check 1",
+          module: Fake,
+          function: :call,
+          result: :ok,
+          time: 1234
+        }
+
+      formatted = Formatter.format(check)
+      assert formatted ==
+        %{
+          "name" => "check 1",
+          "time" => 1234,
+          "healthy" => true,
+          "error" => nil
+        }
+    end
+
+    test "it formats unsuccessful check" do
+      check =
+        %Check{
+          name: "check 1",
+          module: Fake,
+          function: :call,
+          result: {:error, "Error"},
+          time: 1234
+        }
+
+      formatted = Formatter.format(check)
+      assert formatted ==
+        %{
+          "name" => "check 1",
+          "time" => 1234,
+          "healthy" => false,
+          "error" => "Error"
+        }
+    end
+
+    test "it formats a list of checks" do
+      checks =
+        [%Check{
+          name: "check 1",
+          module: Fake,
+          function: :call,
+          result: :ok,
+          time: 1234
+        }]
+
+      formatted = Formatter.format(checks)
+      assert formatted ==
+        [%{
+          "name" => "check 1",
+          "time" => 1234,
+          "healthy" => true,
+          "error" => nil
+        }]
+    end
+  end
+end

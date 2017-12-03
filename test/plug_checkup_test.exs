f96be4b8ead1fecf0fe162f59ff38a4ff0042a1f
Guilherme <ggpasqualino@gmail.com>
2017-12-03 09:45:31 +0100

    Extract plug execution to function

--- test/plug_checkup_test.exs
+++ test/plug_checkup_test.exs
@@ -6,52 +6,50 @@ defmodule PlugCheckupTest do
   alias PlugCheckup.Options
   alias Poison.Parser
 
+  def execute_plug(:healthy) do
+    check = %Check{name: "1", module: MyChecks, function: :execute_successfuly}
+    execute_plug(check)
+  end
+
+  def execute_plug(:not_healthy) do
+    check = %Check{name: "1", module: MyChecks, function: :execute_with_error}
+    execute_plug(check)
+  end
+
+  def execute_plug(check = %Check{}) do
+    options = PlugCheckup.init(%Options{checks: [check]})
+    request = conn(:get, "/")
+
+    PlugCheckup.call(request, options)
+  end
+
   describe "status" do
     test "it is 200 when healthy" do
-      request = conn(:get, "/")
-      options = PlugCheckup.init(%Options{checks: [%Check{name: "1", module: MyChecks, function: :execute_successfuly}]})
-
-      response = PlugCheckup.call(request, options)
-
+      response = execute_plug(:healthy)
       assert response.status == 200
     end
 
     test "it is 500 when not healthy" do
-      request = conn(:get, "/")
-      options = PlugCheckup.init(%Options{checks: [%Check{name: "1", module: MyChecks, function: :execute_with_error}]})
-
-      response = PlugCheckup.call(request, options)
-
+      response = execute_plug(:not_healthy)
       assert response.status == 500
     end
   end
 
   describe "content-type" do
     test "it is 'application/json' when healthy" do
-      request = conn(:get, "/")
-      options = PlugCheckup.init(%Options{checks: [%Check{name: "1", module: MyChecks, function: :execute_successfuly}]})
-
-      response = PlugCheckup.call(request, options)
-
+      response = execute_plug(:healthy)
       assert get_resp_header(response, "content-type") == ["application/json; charset=utf-8"]
     end
 
     test "it is 'application/json' when not healthy" do
-      request = conn(:get, "/")
-      options = PlugCheckup.init(%Options{checks: [%Check{name: "1", module: MyChecks, function: :execute_with_error}]})
-
-      response = PlugCheckup.call(request, options)
-
+      response = execute_plug(:not_healthy)
       assert get_resp_header(response, "content-type") == ["application/json; charset=utf-8"]
     end
   end
 
   describe "body" do
     test "it is a valid json when healthy" do
-      request = conn(:get, "/")
-      options = PlugCheckup.init(%Options{checks: [%Check{name: "1", module: MyChecks, function: :execute_successfuly}]})
-
-      response = PlugCheckup.call(request, options)
+      response = execute_plug(:healthy)
       formatted_response = Parser.parse!(response.resp_body)
 
       assert [%{
@@ -65,10 +63,7 @@ defmodule PlugCheckupTest do
     end
 
     test "it is a valid json when not healthy" do
-      request = conn(:get, "/")
-      options = PlugCheckup.init(%Options{checks: [%Check{name: "1", module: MyChecks, function: :execute_with_error}]})
-
-      response = PlugCheckup.call(request, options)
+      response = execute_plug(:not_healthy)
       formatted_response = Parser.parse!(response.resp_body)
 
       assert [%{

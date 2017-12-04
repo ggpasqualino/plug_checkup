b83e8f0cc38a6b1b27139e400203f8dc805a2a96
Guilherme <ggpasqualino@gmail.com>
2017-12-04 09:36:47 +0100

    Release v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugCheckup.Mixfile do
   def project do
     [
       app: :plug_checkup,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,

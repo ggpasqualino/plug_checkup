9c61387040c001bf080e2a0b2afc8a07c3c0abb5
Jonatan Männchen <jonatan@maennchen.ch>
2021-05-27 12:03:04 +0200

    Resolve Compile Warning and raise Elixir Minimum Requirement to 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PlugCheckup.Mixfile do
     [
       app: :plug_checkup,
       version: "0.6.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       description: description(),
